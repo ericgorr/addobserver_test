59287d227c7e3cdc484388d308a41bd468396847
eric gorr <ericgorr@mac.com>
2015-08-23 15:58:20 -0400

    Initial Commit

---
+++ addobserver_test/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  addobserver_test
+//
+//  Created by Eric Gorr on 8/23/15.
+//  Copyright (c) 2015 Eric Gorr. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
